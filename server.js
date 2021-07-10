34cebd7db0f4aef73788a01316ecd5ce7bc7d8db
qusaiqeisi <qusaiqeisi6@gmail.com>
2021-07-10 16:28:59 +0300

    lab10 done

--- server.js
+++ server.js
@@ -1,3 +1,51 @@
+// 'use strict'
+
+
+// const express = require('express') // require the express package
+// const app = express() // initialize your express app instance
+// const cors = require('cors');
+// require('dotenv').config();
+// app.use(cors()) // after you initialize your express app instance
+// const weatherControl=require('./controller/Weather.Controller')
+// const moviesControl=require('./controller/Movies.Controller')
+
+// const PORT=process.env.PORT
+
+
+// const express = require("express"); // require the express package
+// const app = express(); // initialize your express app instance
+// const weatherData = require("./data/weather.json");
+// const cors = require("cors");
+
+// app.use(cors()); // after you initialize your express app instance
+// require("dotenv").config();
+
+// const PORT = process.env.PORT;
+// // a server endpoint
+// app.get(
+//     "/", // our endpoint name
+//     function(req, res) {
+//         // callback function of what we should do with our request
+//         res.send("Hello World"); // our endpoint function response
+//     }
+// );
+
+// app.get("/weather", (req, res) => {
+//     const newWeatherData = weatherData.data.map((value) => {
+//         return new Weather(value);
+//     });
+//     res.json(newWeatherData);
+// });
+
+// class Weather {
+//     constructor(weatherArr) {
+//         this.description = weatherArr.weather.description;
+//         this.date = weatherArr.valid_date;
+//     }
+// }
+
+// app.listen(PORT); 
+
 'use strict'
 
 
@@ -14,122 +62,13 @@ const PORT=process.env.PORT
 
 
 
-// app.get('/',  (req, res) =>{ 
-//   res.send('Hello World') 
-// })
+app.get('/',  (req, res) =>{ 
+  res.send('Hello World') 
+})
 
 
-// app.get('/weather',weatherControl )
+app.get('/weather',weatherControl )
 
-// app.get('/movies',moviesControl )
+app.get('/movies',moviesControl )
 
-// app.listen(process.env.PORT) 
-
-
-
-
-
-
-// const WEATHER_API_KEY = process.env.WEATHER_API_KEY
-// const MOVIE_API_KEY = process.env.MOVIE_API_KEY
-
-
-// app.get('/weather', (req, res) => {
-//   let weather;
-//   let lat = req.query.lat;
-//   let lon = req.query.lon;
-
-
-//   let url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${WEATHER_API_KEY}&lat=${lat}&lon=${lon}`;
-//   let weatherbitResponde = axios.get(url).then(response => {
-//     weather = response.data;
-//     let forecast = weather.data.map(item => {
-//       return new ForeCast(item)
-//     })
-//     res.json(forecast)
-//   }).catch(err => {
-//     res.status(500).send(`error in getting data ==> ${err}`)
-//   })
-
-// })
-
-
-
-// app.get('/movies', (req, res)=>{
-//   let city=req.query.city
-//  let urlMove=`https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API_KEY}&query=${city}`
-
-//  let moveResponce=axios.get(urlMove).then(response => {
-//    // console.log(response.data.results);
-//    let movie=response.data.results.map(item=>{
-//      return new Movie(item)
-//    })
-//    res.json(movie)
-//  }).catch(err=>{
-//    res.status(500).send(`error in getting data ==> ${err}`)
-// })
-
-
-
-// })
-// class ForeCast {
-//   constructor(weather) {
-
-//     this.date = weather.valid_date
-//     this.description = weather.weather.description
-
-//   }
-// }
-
-
-// class Movie{
-//   constructor(data){
-//     this.average_votes = data.vote_average;
-
-//     this.total_votes=data.vote_count
-
-//     this.image_url=data.backdrop_path
-
-//     this.popularity=data.popularity
-
-//     this.released_on=data.release_date
-   
-// }
-// }
-
-
-
-
-const express = require("express"); // require the express package
-const app = express(); // initialize your express app instance
-const weatherData = require("./data/weather.json");
-const cors = require("cors");
-
-app.use(cors()); // after you initialize your express app instance
-require("dotenv").config();
-
-const PORT = process.env.PORT;
-// a server endpoint
-app.get(
-    "/", // our endpoint name
-    function(req, res) {
-        // callback function of what we should do with our request
-        res.send("Hello World"); // our endpoint function response
-    }
-);
-
-app.get("/weather", (req, res) => {
-    const newWeatherData = weatherData.data.map((value) => {
-        return new Weather(value);
-    });
-    res.json(newWeatherData);
-});
-
-class Weather {
-    constructor(weatherArr) {
-        this.description = weatherArr.weather.description;
-        this.date = weatherArr.valid_date;
-    }
-}
-
-app.listen(PORT); 
+app.listen(process.env.PORT) 
